a079388436a2c9ffd785a2ce01ea467d18e4e1cc
EC2 Default User <ec2-user@ip-172-31-35-40.ec2.internal>
2019-03-13 20:39:03 +0000

    Initial

---
+++ db/migrate/20190313203051_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :descr
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
